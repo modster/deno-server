53d5239727cbaadfafaa326ab0fa0c5da4822120
E. M. Greeff <emgreeff@datadash.cloud>
2023-06-08 09:54:23 -0600

    git init commit

---
+++ ws.ts
@@ -0,0 +1,98 @@
+
+/**
+ * @function WIP
+ * @returns { string } 
+ */
+function uuID() {
+  // to do
+  return Deno.uid();
+}
+
+/**
+ * @function WIP
+ * @returns { string } 
+ */
+function signReqs(r) {
+  // to do
+  return "jgfgg"
+}
+/** @param { Object } ping */
+const ping = {
+  id: Deno.uid(),
+  method: "ping"
+}
+
+/**
+ * @function handle
+ * @param {Deno.Conn } conn 
+ */
+async function handle(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn)
+  {
+    await requestEvent.respondWith(handleReq(requestEvent.request));
+  }
+}
+
+/**
+ * @function handleReq
+ * @param { Request } req 
+ * @returns {Response} response
+ */
+function handleReq(req: Request): Response {
+  const upgrade = req.headers.get("upgrade") || "";
+  if (upgrade.toLowerCase() != "websocket")
+  {
+    return new Response("request isn't trying to upgrade to websocket.");
+  }
+  /**
+   * @constructor 
+   * @returns { socket, response }
+   */
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  /**
+   * @event onopen
+   */
+  socket.onopen = () => {
+    /**
+ * confirm socket is open 
+ */
+    console.log("socket opened");
+    /**
+     * @todo set up cron or interval every 30 minutes
+     * confirm socket is open and test connectivity with a ping
+     */
+    socket.send(JSON.stringify(ping));
+  }
+
+  /**
+   * @event onmessage
+   * @param e 
+   */
+  socket.onmessage = (e) => {
+    console.log(e.data);
+    /**
+     * @todo if a ping is recieved, send pong
+     */
+    // if (e.data.method === "ping") {
+    // socket.send(JSON.stringify(pong));
+    // }
+  };
+
+  /**
+   * @event onerror
+   * @param { Error } e 
+   */
+  socket.onerror = (e) => console.error("Error: ", e);
+
+  /**
+   * @event onclose
+   */
+  socket.onclose = () => console.log("socket closed");
+
+  /**
+   * @returns { Response } response
+   */
+  return response;
+}
